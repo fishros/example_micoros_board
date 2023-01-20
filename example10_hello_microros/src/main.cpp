ca8ecc2074ab6b245f81de61edcff00e8080be96
鱼香ROS <87068644+fishros@users.noreply.github.com>
2023-01-21 01:23:56 +0800

    feat: 添加hello_microros

---
+++ example10_hello_microros/src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <micro_ros_platformio.h>
+
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+
+void setup()
+{
+  Serial.begin(115200);
+  // 设置通过串口进行MicroROS通信
+  set_microros_serial_transports(Serial);
+  // 延时时一段时间，等待设置完成
+  delay(2000);
+  // 初始化内存分配器
+  allocator = rcl_get_default_allocator();
+  // 创建初始化选项
+  rclc_support_init(&support, 0, NULL, &allocator);
+  // 创建节点 hello_microros
+  rclc_node_init_default(&node, "hello_microros", "", &support);
+  // 创建执行器
+  rclc_executor_init(&executor, &support.context, 1, &allocator);
+}
+
+void loop()
+{
+  delay(100);
+  // 循环处理数据
+  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+}
